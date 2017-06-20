7193ab56b3e3eada5af6b8ce55663b4eb296924c
takano yuusaku <justc001@i.sontbank.jp>
2017-06-21 02:34:39 +0900

    fixed server script.

---
+++ app/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const server = http.Server(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/lib', express.static(process.env.PWD + '/app/lib'));
+app.use('/dest', express.static(process.env.PWD + '/app/dest'));
+
+app.get('/', function(req, res) {
+    const stream = fs.createReadStream(process.env.PWD + '/app/index.html', { encoding: 'utf8' });
+    stream.pipe(res);
+});
+
+server.listen(PORT, function() {
+    console.log('server running ...  at heroku app:' + PORT);
+});
