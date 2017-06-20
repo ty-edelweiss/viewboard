cb4c2749524445497ba6219c15b9b92735565321
takano yuusaku <justc001@i.sontbank.jp>
2017-06-21 01:53:36 +0900

    fixed server script.

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const server = http.Server(app);
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/lib', express.static('app/lib'));
-app.use('/dest', express.static('app/dest'));
+app.use('/lib', express.static(__dirname + '/app/lib'));
+app.use('/dest', express.static(__dirname + '/app/dest'));
 
 app.get('/', function(req, res) {
-    const stream = fs.createReadStream('app/index.html', { encoding: 'utf8' });
+    const stream = fs.createReadStream(__dirname + '/app/index.html', { encoding: 'utf8' });
     stream.pipe(res);
 });
 
 server.listen(PORT, function() {
-    console.log('server running ...  at heroku app:' + PORT + __dirname);
+    console.log('server running ...  at heroku app:' + PORT);
 });
